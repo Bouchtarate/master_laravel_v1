2a4d7b4551994e69d08b314b64e910de0d5aa4a7
Hamza Bouchtarate <hamza.bouchtarate@gmail.com>
2023-04-07 23:54:41 +0000

    start working with models

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CrudController;
 use App\Http\Controllers\front\FrontController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -33,3 +34,5 @@ Route::get('front/landing', [FrontController::class, 'landing']);
 Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [CrudController::class, 'index']);
